42b30829d26258d4e24aaccfed3b49dc2c9a9b02
thecoder911 <f20190212@pilani.bits-pilani.ac.in>
2021-07-26 11:15:25 +0530

    Some new files

---
+++ 26 July 2021/1323. Maximum 69 Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        a = list(str(num))
+        for i  in range(len(a)):
+            if a[i] == '6':
+                a[i] = '9'
+                break
+        s = ""
+        for i in a:
+            s += i
+        return int(s)
---
+++ 26 July 2021/561. Array Partition I.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrayPairSum(self, s: List[int]) -> int:
+        s = sorted(s)
+        su = 0
+        i = 0
+        while(i<len(s)):
+            su += min(s[i],s[i+1])
+            i += 2
+        return su
