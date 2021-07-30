d09634ef9fdcf1e49c6a191368cd208ccef067f8
thecoder911 <f20190212@pilani.bits-pilani.ac.in>
2021-07-30 11:50:17 +0530

    some more files

---
+++ 30 July 2021/Deepest sum leave.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        sums = []
+        def dfs(node: TreeNode, lvl: int):
+            if lvl == len(sums): sums.append(node.val)
+            else: sums[lvl] += node.val
+            if node.left: dfs(node.left, lvl+1)
+            if node.right: dfs(node.right, lvl+1)
+        dfs(root, 0)
+        return sums[-1]
